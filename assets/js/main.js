eb0233c8845bd44cca1a52a53819cd0178dc7977
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2022-11-21 11:39:16 -0300

    Updating the js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,47 @@ function closeMenu () {
     closeMobile.style.display = "none";
     subMenu.style.display = "none";
 }
+
+
+// Submenu arrows
+var arrowDown = document.querySelector(".nav__arrow--down");
+var arrowDowns = document.querySelector(".nav__arrow--downs");
+
+var subList = document.querySelector(".nav__ul--son");
+var companyList = document.querySelector(".nav__ul--company");
+
+var arrowUp = document.querySelector(".nav__arrow--up");
+var arrowUps = document.querySelector(".nav__arrow--ups");
+
+// Primeiro submenu
+arrowDown.addEventListener("click", arrowOpen);
+function arrowOpen () {
+    subList.style.display = "block";
+    arrowUp.style.display = "block";
+    arrowDown.style.display = "none";
+}
+
+
+arrowUp.addEventListener("click", arrowClose);
+function arrowClose () {
+    subList.style.display = "none";
+    arrowUp.style.display = "none";
+    arrowDown.style.display = "inline";
+}
+
+
+// Segundo submenu
+arrowDowns.addEventListener("click", arrowTop);
+function arrowTop () {
+    companyList.style.display = "block";
+    arrowUps.style.display = "block";
+    arrowDowns.style.display = "none";
+}
+
+
+arrowUps.addEventListener("click", arrowbottom)
+function arrowbottom () {
+    companyList.style.display = "none";
+    arrowUps.style.display = "none";
+    arrowDowns.style.display = "inline";
+}
